ee9ba7e36c93f15ef1ebc295b29f3628c01e0aa4
vasl43 <155532367+vasl43@users.noreply.github.com>
2024-01-30 17:20:01 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+import { UserContextProvider } from "./userContext";
+import Privacy from "./pages/Privacy";
+import CardPage from "./pages/CardPage";
+import AccountPage from "./pages/AccountPage";
+import OrderPage from "./pages/OrderPage";
+
+axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.withCredentials = true;
+
+function App() {
+    return (
+        <UserContextProvider>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<IndexPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/card/:id" element={<CardPage />} />
+                    <Route path="/privacy" element={<Privacy />} />
+                    <Route path="/account" element={<AccountPage />} />
+                    <Route path="/order" element={<OrderPage />} />
+                </Route>
+            </Routes>
+        </UserContextProvider>
+    );
+}
+
+export default App;
