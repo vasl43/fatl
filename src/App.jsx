0d9ebc566e5c55e36a546c400e980d949b57f438
vasl43 <155532367+vasl43@users.noreply.github.com>
2024-02-10 12:15:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,26 @@ import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
 import RegisterPage from "./pages/RegisterPage";
 import axios from "axios";
-import { UserContextProvider } from "./userContext";
 import Privacy from "./pages/Privacy";
 import CardPage from "./pages/CardPage";
 import AccountPage from "./pages/AccountPage";
-import OrderPage from "./pages/OrderPage";
 
 axios.defaults.baseURL = "https://test.isroil-holding.uz";
 axios.defaults.withCredentials = true;
 
 function App() {
-  return (
-    <UserContextProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<IndexPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/card/:id" element={<CardPage />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/order" element={<OrderPage />} />
-        </Route>
-      </Routes>
-    </UserContextProvider>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<IndexPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/card/:id" element={<CardPage />} />
+                <Route path="/privacy" element={<Privacy />} />
+                <Route path="/account" element={<AccountPage />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
