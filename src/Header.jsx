b96dc86c93cca2fd90fca93f39accab2e6e5fbb0
vasl43 <155532367+vasl43@users.noreply.github.com>
2024-02-10 12:15:12 +0300

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,34 +1,31 @@
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa6";
-import { useContext } from "react";
-import { UserContext } from "./userContext";
+const user = JSON.parse(localStorage.getItem("user")) || null;
 
 export default function Header() {
-  const { user, setUser } = useContext(UserContext);
-
-  return (
-    <header className="max-w-screen-lg m-auto flex justify-between p-4 pt-8 z-50 bg-white">
-      <div className="size-8">
-        <Link to={"/"} className="size-8">
-          <img src="/logo.png" alt="Логотип" />
-        </Link>
-      </div>
-
-      <div className="flex gap-2 items-center cursor-pointer">
-        {!!user && (
-          <Link to={"/account"}>
-            <div className="font-bold text-lg flex items-center">
-              {user.name.split(" ")[0]}
-              <FaUser className="size-7 text-black ml-2" />
+    return (
+        <header className="max-w-screen-lg m-auto flex justify-between p-4 pt-8 z-50 bg-white">
+            <div className="size-8">
+                <Link to={"/"} className="size-8">
+                    <img src="/logo.png" alt="Логотип" />
+                </Link>
             </div>
-          </Link>
-        )}
-        {!user && (
-          <Link to={"/login"}>
-            <FaUser className="size-7 text-black" />
-          </Link>
-        )}
-      </div>
-    </header>
-  );
+
+            <div className="flex gap-2 items-center cursor-pointer">
+                {!!user && (
+                    <Link to={"/account"}>
+                        <div className="font-bold text-lg flex items-center">
+                            {user.fullname.split(" ")[0]}
+                            <FaUser className="size-7 text-black ml-2" />
+                        </div>
+                    </Link>
+                )}
+                {!user && (
+                    <Link to={"/login"}>
+                        <FaUser className="size-7 text-black" />
+                    </Link>
+                )}
+            </div>
+        </header>
+    );
 }
