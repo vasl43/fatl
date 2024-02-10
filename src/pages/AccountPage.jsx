204560301f41d3f42e2e52e78152a9a9560d68c7
vasl43 <155532367+vasl43@users.noreply.github.com>
2024-02-10 12:15:37 +0300

    Update AccountPage.jsx

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,53 +1,58 @@
+const user = JSON.parse(localStorage.getItem("user")) || null;
 import axios from "axios";
-import { useState } from "react";
-import { useContext } from "react";
-import { UserContext } from "../userContext";
 
 export default function AccountPage() {
-    const { user, setUser } = useContext(UserContext);
+    async function payment(ev) {
+        ev.preventDefault();
+        let config = {
+            method: "post",
+            url: "https://test.isroil-holding.uz/api/payment",
+            headers: {
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM5MmMyNyIsImZ1bGxuYW1lIjoiT3liZWsgQWJkdWphYmJvcm92IiwiYnJpdGhkYXkiOiIxOTk2LTAzLTAzVDE5OjAwOjAwLjAwMFoiLCJwaG9uZSI6Iis5OTg5NTAyNzA0OTYiLCJwYXltZW50X2Ftb3VudCI6Mi4xNSwicm9sZSI6ImFkbWluIiwicGFzc3dvcmQiOiI4YzY5NzZlNWI1NDEwNDE1YmRlOTA4YmQ0ZGVlMTVkZmIxNjdhOWM4NzNmYzRiYjhhODFmNmYyYWI0NDhhOTE4IiwiZXhwaXJhdGlvbiI6IjIwMjQtMDMtMDhUMTk6MDA6MDAuMDAwWiIsImRlbGV0ZWRfYXQiOm51bGwsImpvaW5lZF9hdCI6IjIwMjQtMDItMDZUMTU6MDA6MTAuMDAwWiIsInVwZGF0ZWRfYXQiOm51bGwsImlhdCI6MTcwNzQ2Nzk5NCwiZXhwIjoxNzA3NTExMTk0fQ.BKI0r3dOUAu0C8Wt9V657TtixVxas0aqHkA5fbvj0c0",
+            },
+        };
 
-    async function logout() {
-        await axios.post("/logout");
-        setUser(null);
-        window.open("/", "_self");
+        axios
+            .request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-
     return (
         <div className="p-4 max-w-screen-lg m-auto mt-2 lg:mt-10 md:mt-10 sm:mt-10 pt-20">
             {!!user && (
                 <div>
                     <div className="m-auto rounded-xl border-solid border-2 p-4 w-full lg:max-w-xs md:max-w-xs sm:max-w-xs">
                         <div className="flex justify-between items-center">
-                            <p>До конца абонемента:</p>
+                            <p>Действителен до:</p>
                             <p className="flex items-center justify-center">
                                 <label className="text-4xl font-bold mr-3 price">
-                                    {user.left}
+                                    {user.expiration}
                                 </label>
-                                дней
                             </p>
                         </div>
                     </div>
                     <div className="m-auto text-center">
-                        {user.price !== 0 && (
-                            <button className="hover:bg-zinc-800 border-none outline-none shadow-none text-white bg-black p-4 rounded-xl w-full lg:max-w-xs md:max-w-xs sm:max-w-xs transition duration-200 font-bold my-5">
-                                Продлить
-                            </button>
-                        )}
+                        <button
+                            onClick={payment}
+                            className="hover:bg-zinc-800 border-none outline-none shadow-none text-white bg-black p-4 rounded-xl w-full lg:max-w-xs md:max-w-xs sm:max-w-xs transition duration-200 font-bold my-5"
+                        >
+                            Продлить
+                        </button>
                     </div>
                     <p className="text-center text-xl flex justify-center items-center mt-5">
                         Стоимость продления:
                         <label className="font-bold text-3xl ml-2">
-                            {user.price + " ₽"}
+                            {user.payment_amount + " ₽"}
                         </label>
-                    </p>
-                    <p
-                        onClick={logout}
-                        className="text-center cursor-pointer mt-10"
-                    >
-                        Выйти
                     </p>
                 </div>
             )}
+            {!user && window.open("https://atletikum.ru/login", "_self")}
         </div>
     );
 }
