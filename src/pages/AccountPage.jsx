b496a5712cb6e333ad18fcdbcbc7698c247f7aca
vasl43 <155532367+vasl43@users.noreply.github.com>
2024-02-20 15:12:18 +0300

    Update AccountPage.jsx

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,14 +1,38 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 const user = JSON.parse(localStorage.getItem("user")) || null;
 
 export default function AccountPage() {
+    const [userData, setUserData] = useState([]);
     const [buttonText, setButtonText] = useState("Продлить");
     const oneDay = 24 * 60 * 60 * 1000;
     const firstDate = new Date();
-    const secondDate = new Date(user.expiration);
+    const secondDate = new Date(userData.expiration);
     const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+
+    useEffect(() => {
+        let config = {
+            method: "get",
+            maxBodyLength: Infinity,
+            url:
+                "https://test.isroil-holding.uz/api/user/" +
+                JSON.parse(localStorage.getItem("user"))?.id,
+            headers: {
+                Authorization:
+                    "Bearer " + JSON.parse(localStorage.getItem("user"))?.token,
+            },
+        };
+
+        axios
+            .request(config)
+            .then((response) => {
+                setUserData(response.data.innerData);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     async function payment(ev) {
         ev.preventDefault();
@@ -24,13 +48,12 @@ export default function AccountPage() {
             url: "https://test.isroil-holding.uz/api/payment",
             headers: {
                 Authorization:
-                    "Bearer " + JSON.parse(localStorage.getItem("user")).token,
+                    "Bearer " + JSON.parse(localStorage.getItem("user"))?.token,
             },
         };
         axios
             .request(config)
             .then((response) => {
-                localStorage.clear();
                 const confirmation_url =
                     response.data.innerData.confirmation_url;
                 window.open(confirmation_url, "_self");
@@ -46,7 +69,6 @@ export default function AccountPage() {
         localStorage.clear();
         window.open("https://atletikum.ru/", "_self");
     }
-
     return (
         <div className="p-4 max-w-screen-lg m-auto mt-2 lg:mt-10 md:mt-10 sm:mt-10 pt-20">
             {!!user && (
@@ -54,7 +76,7 @@ export default function AccountPage() {
                     <div className="m-auto rounded-xl border-solid border-2 p-4 w-full lg:max-w-xs md:max-w-xs sm:max-w-xs">
                         <div className="flex justify-center items-center">
                             <p className="flex items-center justify-center">
-                                {user.expiration != null && (
+                                {userData.expiration != null && (
                                     <label className="text-lg font-regular mr-3 price flex">
                                         {"Осталось "}
                                         {diffDays}
@@ -63,7 +85,7 @@ export default function AccountPage() {
                                         {" дней"}
                                     </label>
                                 )}
-                                {user.expiration == null && (
+                                {userData.expiration == null && (
                                     <label className="text-xl font-regular mr-3 price text-center">
                                         Нет активного абонемента
                                     </label>
@@ -82,7 +104,7 @@ export default function AccountPage() {
                     <p className="text-center text-lg flex justify-center items-center mt-5 mb-5">
                         Стоимость продления:
                         <label className="font-regular text-xl ml-2">
-                            {user.payment_amount + " ₽"}
+                            {userData.payment_amount + " ₽"}
                         </label>
                     </p>
                     <div className="flex justify-center items-center text-center m-auto py-10">
